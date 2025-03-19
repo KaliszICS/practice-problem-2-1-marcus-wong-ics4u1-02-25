14530e9ecf5656f105214aa039c85d994baf5ae9
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-03-19 14:21:42 +0000

    same assignment 2.1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,17 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int find(int[] arr, int num) {
+		for (int i  = 0; i < arr.length; i++) {
+			if (arr[i] == num) {
+				return i;
+
+			}
+			
+		}
+		return -1;
+
+	}
 
 	
 
