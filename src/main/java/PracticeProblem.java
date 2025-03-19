b7580ede9500f6f861009a80557cabe51130f8a8
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-03-19 14:56:43 +0000

    same assignment 2.1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -13,6 +13,37 @@ public class PracticeProblem {
 		}
 		return -1;
 
+	} 
+	public static int findLast(String[] arr, String str) {
+		for (int i = arr.length - 1; i >= 0; i--) {
+			if (arr[i].equals(str)) {
+				return i; 
+
+			}
+		}
+		return -1;
+
+	}
+	public static int findSecond(char[] arr, char c) {
+		int counter = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == c) {
+				counter++;
+				if (counter == 2) {
+					return i;
+
+				}
+				
+
+			}
+
+		}
+		if (counter == 1) {
+			return arr.length - 1;
+
+		}
+		return -1;
+
 	}
 
 	
